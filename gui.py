27b0c918b832e8707ffb650c4dd658a48b38b911
cde8eae8 <nikitadrozd@outlook.com>
2020-09-25 21:27:29 +0300

    .

--- gui.py
+++ gui.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import threading
-from widgets import *
 
 class Gui(QMainWindow):
     def __init__(self, callback, widget_types, *args):

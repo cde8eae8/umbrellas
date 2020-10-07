05aa14de6e606b0ddafa530fa8bb241b9ff0ce9a
cde8eae8 <nikitadrozd@outlook.com>
2020-10-07 16:11:46 +0300

    fixed relative paths - now absolute

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from gui import *
 from PyQt5.QtWidgets import *
 import subprocess
 import shlex
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 import sys
-sys.path.insert(0,'..')
+sys.path.insert(0,os.path.join(SCRIPT_DIR, '..'))
 import update
 
 app = QApplication([])
@@ -13,7 +16,7 @@ if update.has_new_version():
     print('new version!')
     ans = QMessageBox.question(None, 'Update', 'Доступна новая версия. Скачать?')
     if (ans == QMessageBox.Yes):
-        args = shlex.split('python3 ../update.py')
+        args = shlex.split(f'{sys.executable} {os.path.join(SCRIPT_DIR, "../update.py")}')
         subprocess.Popen(args, start_new_session=True)
         sys.exit(0)
 

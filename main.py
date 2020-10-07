8b62e7cbd3bb446e124da0bcb784433eca7319cd
cde8eae8 <nikitadrozd@outlook.com>
2020-10-07 16:27:13 +0300

    fixed shlex argument parsing on windows

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ if update.has_new_version():
     print('new version!')
     ans = QMessageBox.question(None, 'Update', 'Доступна новая версия. Скачать?')
     if (ans == QMessageBox.Yes):
-        args = shlex.split(f'{sys.executable} {os.path.join(SCRIPT_DIR, "../update.py")}')
+        #args = shlex.split(f'{sys.executable} {os.path.join(SCRIPT_DIR, "../update.py")}')
+        args = [sys.executable, os.path.join(SCRIPT_DIR, "../update.py")]
         subprocess.Popen(args, start_new_session=True)
         sys.exit(0)
 
